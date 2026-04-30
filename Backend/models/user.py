382089fff303747e3bf8cba0bfdf6de2c67040de
ishachenna09 <ishachenna26@gmail.com>
2026-04-30 20:31:41 +0530

    Isha (#5)
    * Added files
    
    * databases connectivity created

---
+++ Backend/models/user.py
@@ -0,0 +1,12 @@
+from database import Base, engine, SessionLocal 
+from sqlalchemy import Column, Integer, String
+
+
+
+class User(Base):
+    __tablename__ = 'users'
+    
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    emailid = Column(String, unique=True, index=True)
+    password = Column(String)
