a22a6831ba596428baf45b3fc93d144ebdc5c4b8
neerajsbhandari27 <neerajsbhandari27@gmail.com>
2026-05-04 16:54:37 +0530

    "frontend"

--- Backend/database.py
+++ Backend/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 # SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:supabase9302@db.mjhuiizwxfwznfpfzywp.supabase.co:5432/postgres'
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres123@localhost:5432/mydb"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:neeraj@localhost:5432/mydb"
 engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
